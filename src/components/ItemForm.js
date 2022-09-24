c7ed7076f7683b4ed2e8a0a57f25e4d70437ff86
Osamu Ito <mr.samito@gmail.com>
2022-09-23 21:13:19 -0400

    commit tree df7af5f80325b814c575ff72e44611f2033ccae2

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+// _When the form is submitted_, a new item should be created
+// and added to our list of items.
+// Make all the input fields for this form controlled inputs, so that you can
+//   access all the form data via state. When setting the initial state for the
+//   `<select>` tag, use an initial value of "Produce" (since that's the first
+//   option in the list).
+
+function ItemForm({onItemFormSubmit}) {
+  const [newItemData, setNewItemData ] = useState({
+    name: "",
+    category: "Produce",
+  })
+  const handleChange = (e) => {
+    setNewItemData({
+     ...newItemData,
+      [e.target.name]: e.target.value,
+    })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onItemFormSubmit({
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    ...newItemData
+  });
+  }
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
